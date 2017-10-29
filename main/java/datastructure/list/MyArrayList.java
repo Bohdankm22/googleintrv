502091e2f3ba815b5bb3221df0d575f6db1c8c5a
Bohdansh <bohdansharipov@gmail.com>
2017-10-29 14:30:37 -0400

    Added tests for simple inputs in Array list.

--- main/java/datastructure/list/MyArrayList.java
+++ main/java/datastructure/list/MyArrayList.java
@@ -2,4 +2,7 @@ package datastructure.list;
 
 public class MyArrayList<T> {
 
+    public boolean add(T item) {
+        return false;
+    }
 }
--- test/java/datastructure/list/TestMyArrayList.java
+++ test/java/datastructure/list/TestMyArrayList.java
@@ -1,5 +1,23 @@
 package datastructure.list;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestMyArrayList {
+
+    @Test
+    public void testStringInput() {
+        MyArrayList<String> test = new MyArrayList<>();
+        String testArg = "test";
+        assertEquals("Simple check should be equals", test.add(testArg), true);
+    }
+
+    @Test
+    public void testIntegerInput() {
+        MyArrayList<Integer> test = new MyArrayList<>();
+        int testArg = Integer.MAX_VALUE;
+        assertEquals("Simple check should be equals", test.add(testArg), true);
+    }
 
 }
