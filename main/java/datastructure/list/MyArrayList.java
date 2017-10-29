b63156dab08d947c057fcd9c25978d485f194676
Bohdansh <bohdansharipov@gmail.com>
2017-10-29 14:46:28 -0400

    Implemented adding elements to the array list.

--- main/java/datastructure/list/MyArrayList.java
+++ main/java/datastructure/list/MyArrayList.java
@@ -2,7 +2,25 @@ package datastructure.list;
 
 public class MyArrayList<T> {
 
+    private static final int DEFAULT_ARRAY_INIT_SIZE = 12;
+    private Object[] internalArray;
+    private int lastPosition;
+    private int arraySize;
+
+    public MyArrayList() {
+        internalArray = new Object[DEFAULT_ARRAY_INIT_SIZE];
+        lastPosition = 0;
+        arraySize = DEFAULT_ARRAY_INIT_SIZE;
+    }
+
     public boolean add(T item) {
-        return false;
+        internalArray[lastPosition] = item;
+        if (lastPosition / arraySize == 1) {
+            arraySize = (int) Math.round(arraySize * 1.5);
+            Object[] tmp = new Object[arraySize];
+            System.arraycopy(internalArray, 0, tmp, 0, lastPosition + 1);
+            internalArray = tmp;
+        }
+        return true;
     }
 }
