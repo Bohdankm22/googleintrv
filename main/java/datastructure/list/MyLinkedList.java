47ba7ad53bfec47b90ef65e1cc79aa8da03bd896
Bohdansh <bohdansharipov@gmail.com>
2017-10-29 20:52:10 -0400

    Implemented adding at the end. Added a test.

--- main/java/datastructure/list/MyLinkedList.java
+++ main/java/datastructure/list/MyLinkedList.java
@@ -9,7 +9,20 @@ public class MyLinkedList<T> {
     private class Node {
         T elem;
         Node next;
+
+        public Node(T elem) {
+            this.elem = elem;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
+
     public MyLinkedList() {
         size = 0;
     }
@@ -22,5 +35,16 @@ public class MyLinkedList<T> {
         return size() == 0;
     }
 
+    public boolean add(T item) {
+        Node addedNode = new Node(item);
+        if (size == 0) {
+            first = addedNode;
+        } else {
+            last.setNext(addedNode);
+        }
+        last = addedNode;
+        size++;
+        return true;
+    }
 
 }
--- test/java/datastructure/list/TestMyLinkedList.java
+++ test/java/datastructure/list/TestMyLinkedList.java
@@ -12,4 +12,16 @@ public class TestMyLinkedList {
         assertEquals("The list should be empty.", true, test.empty());
         assertEquals("The list should have 0 elements.", 0, test.size());
     }
+
+    @Test
+    public void testAddElement() {
+        MyLinkedList<Integer> test = new MyLinkedList<>();
+        assertEquals("The list should be empty.", true, test.empty());
+        assertEquals("The list should have 0 elements.", 0, test.size());
+        for (int i = 1; i < 100; i++) {
+            test.add(i);
+            assertEquals("The list should be empty.", false, test.empty());
+            assertEquals("The list should have 0 elements.", i, test.size());
+        }
+    }
 }
