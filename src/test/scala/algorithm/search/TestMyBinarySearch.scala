8e6da5eec677f9814085b00dea499d46cc797dc6
Bohdansh <bohdansharipov@gmail.com>
2017-11-07 11:22:22 -0500

    Fixed formatting.

--- src/test/scala/algorithm/search/TestMyBinarySearch.scala
+++ src/test/scala/algorithm/search/TestMyBinarySearch.scala
@@ -1,10 +1,9 @@
 package algorithm.search
 
-import algorithm.search.MyBinarySearch.search
-import algorithm.search.MyBinarySearch.searchRecursively
+import algorithm.search.MyBinarySearch.{search, searchRecursively}
 import org.scalatest.FlatSpec
 
-class TestMyBinarySearch extends FlatSpec{
+class TestMyBinarySearch extends FlatSpec {
 
   val arr = Array(1, 5, 7, 9, 25, 26, 30, 42, 96)
   "A search(loop)" should "find the values that exist in the array" in {
