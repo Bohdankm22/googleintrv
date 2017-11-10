7ca886fccea13ebb6bd910cabeeb23e0d77ab480
Bohdansh <bohdansharipov@gmail.com>
2017-11-09 20:31:40 -0500

    Added merge sort class for later implementation.

---
+++ src/main/java/algorithms/sort/MyMergeSort.java
@@ -0,0 +1,11 @@
+package algorithms.sort;
+
+import datastructure.list.MyList;
+
+public class MyMergeSort<T extends Comparable<T>> implements Sortable<T> {
+
+    @Override
+    public MyList<T> sort(MyList<T> list) {
+        return null;
+    }
+}
