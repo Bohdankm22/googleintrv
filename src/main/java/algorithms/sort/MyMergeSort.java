5c375bd71674978c99e5ebd21eb944893088f25d
Bohdansh <bohdansharipov@gmail.com>
2017-11-11 22:03:20 -0500

    Added doc fo merge sort.

--- src/main/java/algorithms/sort/MyMergeSort.java
+++ src/main/java/algorithms/sort/MyMergeSort.java
@@ -2,6 +2,13 @@ package algorithms.sort;
 
 import datastructure.list.MyList;
 
+/**
+ * Merge sort is an efficient, general-purpose, comparison-based sorting algorithm.
+ * Worst case scenario O(n*log(n)).
+ * Best case scenario O(n*log(n)).
+ * Average case scenario O(n*log(n)).
+ * Memory (auxiliary space) O(n).
+ */
 public class MyMergeSort<T extends Comparable<T>> implements Sortable<T> {
 
     @Override
