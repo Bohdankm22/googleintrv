881222d15e6f2791334f2ab823b89442e5a977d2
Bohdansh <bohdansharipov@gmail.com>
2017-11-14 15:04:35 -0500

    Added IMyTree interface for tree data structure.

---
+++ src/main/java/datastructure/IMyTree.java
@@ -0,0 +1,14 @@
+package datastructure;
+
+public interface IMyTree<K, V> {
+
+    IMyTree<K, V> put(K key, V value);
+
+    V get(K key);
+
+    boolean containsKey(K key);
+
+    boolean empty();
+
+    int size();
+}
