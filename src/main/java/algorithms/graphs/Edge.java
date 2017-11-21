1437ba8f52b78bb08ef80427cc79af8112c1d4f5
Bohdansh <bohdansharipov@gmail.com>
2017-11-20 19:23:04 -0500

    Implemented an Edge class.

--- src/main/java/algorithms/graphs/Edge.java
+++ src/main/java/algorithms/graphs/Edge.java
@@ -1,4 +1,20 @@
 package algorithms.graphs;
 
 public class Edge {
+
+    private Vertex oneVertex;
+    private Vertex secVertex;
+
+    public Edge(Vertex oneVertex, Vertex secVertex) {
+        this.oneVertex = oneVertex;
+        this.secVertex = secVertex;
+    }
+
+    public Vertex getOneVertex() {
+        return oneVertex;
+    }
+
+    public Vertex getSecVertex() {
+        return secVertex;
+    }
 }
