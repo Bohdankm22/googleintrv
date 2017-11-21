570b3d8cd1053daa8ce9d5e08d748f601e40e0b2
Bohdansh <bohdansharipov@gmail.com>
2017-11-20 19:24:28 -0500

    Added cost tot he edge representation.

--- src/main/java/algorithms/graphs/Edge.java
+++ src/main/java/algorithms/graphs/Edge.java
@@ -4,10 +4,16 @@ public class Edge {
 
     private Vertex oneVertex;
     private Vertex secVertex;
+    private long cost;
 
     public Edge(Vertex oneVertex, Vertex secVertex) {
+        this(oneVertex, secVertex, 1);
+    }
+
+    public Edge(Vertex oneVertex, Vertex secVertex, long cost) {
         this.oneVertex = oneVertex;
         this.secVertex = secVertex;
+        this.cost = cost;
     }
 
     public Vertex getOneVertex() {
