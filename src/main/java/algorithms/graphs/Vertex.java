728efcf3d5a06c7d023517f61cd7e820e1b9b225
Bohdansh <bohdansharipov@gmail.com>
2017-11-20 19:26:46 -0500

    Implemented Vertex class.

--- src/main/java/algorithms/graphs/Vertex.java
+++ src/main/java/algorithms/graphs/Vertex.java
@@ -2,5 +2,11 @@ package algorithms.graphs;
 
 public class Vertex {
 
+    private static long generatedId = 0;
 
+    private long id;
+
+    public Vertex() {
+        this.id = ++generatedId;
+    }
 }
