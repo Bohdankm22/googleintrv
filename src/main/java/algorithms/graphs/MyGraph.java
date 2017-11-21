c70b23d39c485b2d05b765f1abedc69032022613
Bohdansh <bohdansharipov@gmail.com>
2017-11-20 19:35:29 -0500

    Implemented checkAllVertexRelated.

--- src/main/java/algorithms/graphs/MyGraph.java
+++ src/main/java/algorithms/graphs/MyGraph.java
@@ -19,6 +19,20 @@ public class MyGraph {
     public MyList<Edge> getEdgeList() {
         return edgeList;
     }
+
+    private static boolean checkAllVertexRelated(MyList<Vertex> vertexList, MyList<Edge> edgeList) {
+        for (int i = 0; i < vertexList.size(); i++) {
+            boolean found = false;
+            for (int j = 0; j < edgeList.size(); j++) {
+                if (edgeList.get(j).getOneVertex().equals(vertexList.get(i)) ||
+                        edgeList.get(j).getSecVertex().equals(vertexList.get(i))) {
+                    found = true;
+                }
+            }
+            if (!found) return false;
+        }
+        return true;
+    }
 }
 
 
