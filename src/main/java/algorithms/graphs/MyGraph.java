12a756ac9222be650ae4008912de2090fa9e5fbe
Bohdansh <bohdansharipov@gmail.com>
2017-11-20 19:29:30 -0500

    Implemented the Graph class.

--- src/main/java/algorithms/graphs/MyGraph.java
+++ src/main/java/algorithms/graphs/MyGraph.java
@@ -1,8 +1,24 @@
 package algorithms.graphs;
 
+import datastructure.list.MyList;
+
 public class MyGraph {
 
+    private MyList<Vertex> vertexList;
+    private MyList<Edge> edgeList;
 
+    public MyGraph(MyList<Vertex> vertexList, MyList<Edge> edgeList) {
+        this.vertexList = vertexList;
+        this.edgeList = edgeList;
+    }
+
+    public MyList<Vertex> getVertexList() {
+        return vertexList;
+    }
+
+    public MyList<Edge> getEdgeList() {
+        return edgeList;
+    }
 }
 
 
