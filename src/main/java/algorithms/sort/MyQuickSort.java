4018b12ee53af0a259e4e3de925ac886251d4f36
Bohdansh <bohdansharipov@gmail.com>
2017-11-27 19:32:33 -0500

    Implemented the quick sort algorithms.

--- src/main/java/algorithms/sort/MyQuickSort.java
+++ src/main/java/algorithms/sort/MyQuickSort.java
@@ -2,11 +2,36 @@ package algorithms.sort;
 
 import datastructure.list.MyList;
 
+import static algorithms.sort.MySelectionSort.swap;
+
 public class MyQuickSort<T extends Comparable<T>> implements Sortable<T> {
 
     @Override
     public MyList<T> sort(MyList<T> list) {
-        return null;
+        return list.size() == 0 ? list : sort(list, 0, list.size() - 1);
+    }
+
+    private MyList<T> sort(MyList<T> list, int low, int high) {
+        if (low < high) {
+            int p = partition(list, low, high);
+            sort(list, low, p - 1);
+            sort(list, p + 1, high);
+        }
+        return list;
+    }
+
+    private int partition(MyList<T> list, int low, int high) {
+        T pivot = list.get(high);
+        int j = low - 1;
+        for (int i = low; i < high; i++) {
+            if (list.get(i).compareTo(pivot) < 0) {
+                swap(list, i, ++j);
+            }
+        }
+        if (list.get(high).compareTo(list.get(++j)) < 0) {
+            swap(list, high, j);
+        }
+        return j;
     }
 
     @Override
